84be6240ec05d9ade86a305a27e158932fea08b1
Niladri <rickyrickado14@gmail.com>
2023-08-23 17:31:02 +0530

    maps added

--- src/charts-and-maps.tsx
+++ src/charts-and-maps.tsx
@@ -1,65 +1,100 @@
-import { Line } from 'react-chartjs-2';
+import { Line } from "react-chartjs-2";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useQuery } from "react-query";
+import axios from "axios";
 import Navbar from "./components/navbar";
 
-import {
-  Chart as ChartJS,
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  Legend,
-  Tooltip
-} from "chart.js";
-
-ChartJS.register(
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  Legend,
-  Tooltip
-);
-
 function ChartsAndMaps() {
+  const { data: worldData } = useQuery("worldData", () =>
+    axios
+      .get("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.data)
+  );
+
+  const { data: countryData } = useQuery("countryData", () =>
+    axios
+      .get("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.data)
+  );
+
+  // const formatWorldwideDataForChart = (data) => {
+  //   const labels = Object.keys(data.cases);
+  //   const cases = Object.values(data.cases);
+
+  //   return {
+  //     labels,
+  //     datasets: [
+  //       {
+  //         label: "Worldwide Cases",
+  //         data: cases,
+  //         borderColor: "aqua",
+  //         backgroundColor: "rgba(0, 123, 255, 0.2)",
+  //         fill: true,
+  //       },
+  //     ],
+  //   };
+  // };
+
+  const markers = countryData?.map((country) => ({
+    lat: country.countryInfo.lat,
+    lng: country.countryInfo.long,
+    countryName: country.country,
+    cases: country.cases,
+    deaths: country.deaths,
+  }));
+
   return (
     <div>
-      {/* <Navbar /> */}
-      <Line
-        data={{
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-          datasets: [
-            {
-              label: "# of votes",
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: "aqua",
-              borderColor: "white",
-              pointBorderColor: "aqua",
-              fill: true,
-            },
-          ],
-        }}
-        height={400}
-        width={600}
-        options={{
-          plugins: {
-            legend: {
-              display: true,
-              position: "top",
-              labels: {
-                font: {
-                  size: 18,
+      <Navbar />
+      {/* <div>
+        <Line
+          data={formatWorldwideDataForChart(worldData)}
+          height={400}
+          width={600}
+          options={{
+            plugins: {
+              legend: {
+                display: true,
+                position: "top",
+                labels: {
+                  font: {
+                    size: 18,
+                  },
                 },
               },
             },
-          },
-          maintainAspectRatio: false,
-          scales: {
-            y: {
-              beginAtZero: true,
+            maintainAspectRatio: false,
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
             },
-          },
-        }}
-      />
+          }}
+        />
+      </div> */}
+      <div>
+        <MapContainer
+          center={[20, 0]}
+          zoom={2}
+          style={{ height: "60vh", width: "100%" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          {markers?.map((marker, index) => (
+            <Marker key={index} position={[marker.lat, marker.lng]}>
+              <Popup>
+                <h1>{marker.countryName}</h1>
+                <p>Total Cases: {marker.cases}</p>
+                <p>Total Deaths: {marker.deaths}</p>
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from "@/components/theme-provider";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <App />
   </ThemeProvider>
+  </QueryClientProvider>
 );
